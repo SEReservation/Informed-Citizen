3f0219c76c3a9b71e1bae2a730866af87908d0c6
ar1214 <ar1214git@gmail.com>
2016-04-18 15:11:11 -0500

    Added a bunch of api stuff, getting representative info on state and district level, getting their picture, chamber, money contributed to and paid out on committee and individual level, term begin and end dates, Contact info (website, contact form, facebook, phone)

--- client/src/app/userHome/userHome.js
+++ client/src/app/userHome/userHome.js
@@ -7,4 +7,24 @@ angular.module('userHome', [])
   });
 }])
 
-.controller('UserHomeCtrl');
+.controller('UserHomeCtrl', function ($scope, $http){
+
+  var mylat = 0;
+  var mylong = 0;
+
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+        $scope.latitude = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+        $http.get('http://congress.api.sunlightfoundation.com/legislators/locate?latitude=' + $scope.latitude +'&longitude='+$scope.longitude+'&apikey=efe4a4e77a154868947e0e24593a32e7').success(function (data) {
+          $scope.reports = data.results;
+
+
+        })
+
+      })
+    }
+
+
+
+});
